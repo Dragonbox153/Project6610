cd33e0d044a05e0bd94467186c6a1d901029b43f
Dragonbox153 <u1531795@umail.utah.edu>
2025-04-18 15:27:36 -0600

    Revert unchanges to playerAttackEffect

--- Assets/Scripts/Player/PlayerAttackEffect.cs
+++ Assets/Scripts/Player/PlayerAttackEffect.cs
@@ -6,10 +6,20 @@ public class PlayerAttackEffect : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.GetComponent<EnemyHealth>()  != null)
+        byte bulID = gameObject.GetComponent<Bullet>().GetBulletID();
+        if(collision.GetComponent<EnemyHealth>()  != null && bulletIsType(bulID, 1))
         {
             Debug.Log("Player lands a close hit!");
             collision.GetComponent<EnemyHealth>().TakeDamage(2);
         }
+        if(collision.GetComponent<PlayerHealth>()  != null && bulletIsType(bulID, 2))
+        {
+            collision.GetComponent<PlayerHealth>().TakeDamage(2);
+        }
+    }
+
+    private bool bulletIsType(byte bulID, byte type)
+    {
+        return bulID == 0 || bulID == type;
     }
 }
