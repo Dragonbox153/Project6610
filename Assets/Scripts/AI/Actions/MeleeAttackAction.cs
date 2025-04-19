3912ef1e91781752c3c0b85bcaa99752d622d332
Dragonbox153 <u1531795@umail.utah.edu>
2025-04-19 12:24:47 -0600

    renamed MeleeAtackAction to MeleeAttackAction

--- Assets/Scripts/AI/Actions/MeleeAttackAction.cs
+++ Assets/Scripts/AI/Actions/MeleeAttackAction.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class MeleeAtackAction : Action
+public class MeleeAttackAction : Action
 {
     public EnemyMeleeAttack enemyMeleeAttack;
 
--- Assets/Scripts/AI/Actions/MovementAction.cs
+++ Assets/Scripts/AI/Actions/MovementAction.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public class MovementAction : Action
 {
     public EnemyMovement enemyMovement;
+    public float distanceThreshold;
     public Vector2 targetPosition;
 
     private void Start()
     {
         interupting = false;
-        canDoSimultaneously.Add(typeof(MeleeAtackAction));
+        canDoSimultaneously.Add(typeof(MeleeAttackAction));
     }
 
     public override void Execute()
     {
-        GameObject enemy = enemyMovement.gameObject;
-        enemyMovement.acceleration = new Vector2(targetPosition.x - enemy.transform.position.x, targetPosition.y - enemy.transform.position.y).normalized;
-        enemyMovement.acceleration *= enemyMovement.speed;
+        if(Vector2.Distance(enemyMovement.gameObject.transform.position, targetPosition) > distanceThreshold)
+        {
+            GameObject enemy = enemyMovement.gameObject;
+            enemyMovement.acceleration = new Vector2(targetPosition.x - enemy.transform.position.x, targetPosition.y - enemy.transform.position.y).normalized;
+            enemyMovement.acceleration *= enemyMovement.speed;
+        }
+        else
+        {
+            enemyMovement.acceleration = Vector2.zero;
+        }
+        
     }
 }
