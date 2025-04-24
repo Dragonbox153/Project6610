e2048bc4f32ac537050df3a8b909699a7e550001
gatomon20xx <95043862+gatomon20xx@users.noreply.github.com>
2025-04-24 17:55:13 -0600

    Health and Treasure displayed

---
+++ Assets/TreasureDisplay.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TreasureDisplay : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text HPDisplay, treasureDisplay;
+    [SerializeField]
+    private GameObject player;
+    private GetTreasure treasureCount;
+    private PlayerHealth hpCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        treasureCount = player.GetComponent<GetTreasure>();
+        hpCount = player.GetComponent<PlayerHealth>();
+        treasureDisplay.text = "Treasure: $" + (float)treasureCount.totalTreasure + ".00";
+        HPDisplay.text = "HP: " + (float)hpCount.health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        treasureDisplay.text = "Treasure: $" + (float)treasureCount.totalTreasure + ".00";
+        HPDisplay.text = "HP: " + (float)hpCount.health;
+    }
+}
